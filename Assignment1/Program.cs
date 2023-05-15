e1084fe0d2e43df3abc6c401e3df71bc68066808
Jp8125 <190170116055@vgec1.gujgov.edu.in>
2023-05-15 19:02:05 +0530

    Add project files.

---
+++ Assignment1/Program.cs
@@ -0,0 +1,40 @@
+﻿using Nancy.Json;
+using System;
+using System.IO;
+
+class Test
+{
+public static void Main()
+    {
+         string path1= @"D:\.net core\mod9\Assignment1\movie.json";
+        FileStream fs = new FileStream(path1,FileMode.Open,FileAccess.Read);
+        StreamReader sr=new StreamReader(fs);
+        var demo = sr.ReadToEnd();
+        //Console.WriteLine(demo);
+        var resultObject = new JavaScriptSerializer().Deserialize<mv[]>(demo);
+        foreach (var item in resultObject)
+        {
+            Console.WriteLine(item.MovieName);
+        }
+       
+        
+    }
+}
+ public class mv
+    {
+
+        public int MovieID;
+
+        public string MovieName;
+
+        public Details Details;
+
+    }
+    public class Details
+        {
+            public string DirectorName;
+
+            public string[] ActorsNames;
+
+            public string VideoLink;
+}
